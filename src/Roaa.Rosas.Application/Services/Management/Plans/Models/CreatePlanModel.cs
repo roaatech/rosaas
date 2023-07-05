a4260e6712d7b151a25560adb1deb87877be54a2
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2023-07-05 14:47:57 +0300

    build and implement Plan CROD services

---
+++ src/Roaa.Rosas.Application/Services/Management/Plans/Models/CreatePlanModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Roaa.Rosas.Application.Services.Management.Plans.Models
+{
+    public record CreatePlanModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ src/Roaa.Rosas.Application/Services/Management/Plans/Validators/UpdatePlanValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Roaa.Rosas.Application.Services.Management.Plans.Models;
+using Roaa.Rosas.Authorization.Utilities;
+using Roaa.Rosas.Common.Extensions;
+using Roaa.Rosas.Common.Models.Results;
+using Roaa.Rosas.Common.SystemMessages;
+
+namespace Roaa.Rosas.Application.Services.Management.Plans.Validators
+{
+    public class UpdatePlanValidator : AbstractValidator<UpdatePlanModel>
+    {
+        public UpdatePlanValidator(IIdentityContextService identityContextService)
+        {
+            RuleFor(x => x.Name).NotEmpty().WithError(CommonErrorKeys.ParameterIsRequired, identityContextService.Locale);
+        }
+    }
+}
