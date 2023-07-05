ee14c39a3b2b8102a9c5f74b92c6310f86359d27
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2023-07-05 14:41:48 +0300

    build Plan entity

---
+++ src/Roaa.Rosas.Domain/Entities/Management/Plan.cs
@@ -0,0 +1,10 @@
+﻿namespace Roaa.Rosas.Domain.Entities.Management
+{
+    public class Plan : BaseAuditableEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int DisplayOrder { get; set; }
+        public virtual ICollection<PlanFeature> Features { get; set; }
+    }
+}
