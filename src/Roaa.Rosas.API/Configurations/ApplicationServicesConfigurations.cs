4bad088e232637a6937793ab797ff29267e9f79c
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2023-07-05 14:57:30 +0300

    build and implement PlanFeature endpoints

--- src/Roaa.Rosas.API/Configurations/ApplicationServicesConfigurations.cs
+++ src/Roaa.Rosas.API/Configurations/ApplicationServicesConfigurations.cs
@@ -6,6 +6,7 @@ using Roaa.Rosas.Application.Services.Identity.Accounts;
 using Roaa.Rosas.Application.Services.Identity.Auth;
 using Roaa.Rosas.Application.Services.IdentityServer4.Auth;
 using Roaa.Rosas.Application.Services.Management.Features;
+using Roaa.Rosas.Application.Services.Management.PlanFeatures;
 using Roaa.Rosas.Application.Services.Management.Plans;
 using Roaa.Rosas.Application.Services.Management.Products;
 using Roaa.Rosas.Application.Tenants.Service;
@@ -56,6 +57,7 @@ namespace Roaa.Rosas.Framework.Configurations
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<IFeatureService, FeatureService>();
             services.AddScoped<IPlanService, PlanService>();
+            services.AddScoped<IPlanFeatureService, PlanFeatureService>();
 
             services.AddMediatRAServices();
 
---
+++ src/Roaa.Rosas.API/Controllers/Admin/PlanFeaturesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Roaa.Rosas.Application.Services.Management.PlanFeatures;
+using Roaa.Rosas.Application.Services.Management.PlanFeatures.Models;
+using Roaa.Rosas.Authorization.Utilities;
+using Roaa.Rosas.Framework.Controllers.Common;
+
+namespace Roaa.Rosas.Framework.Controllers.Admin
+{
+    [Route($"{PrefixSuperAdminMainApiRoute}/products/{{productId}}/plans")]
+    public class PlanFeaturesController : BaseSuperAdminMainApiController
+    {
+        #region Props 
+        private readonly ILogger<PlanFeaturesController> _logger;
+        private readonly IPlanFeatureService _planFeatureService;
+        private readonly IIdentityContextService _identityContextService;
+        private readonly IWebHostEnvironment _environment;
+        #endregion
+
+        #region Corts
+        public PlanFeaturesController(ILogger<PlanFeaturesController> logger,
+                                IWebHostEnvironment environment,
+                                IIdentityContextService identityContextService,
+                               IPlanFeatureService planFeatureService)
+        {
+            _logger = logger;
+            _environment = environment;
+            _identityContextService = identityContextService;
+            _planFeatureService = planFeatureService;
+        }
+        #endregion
+
+        #region Actions   
+
+        [HttpGet()]
+        public async Task<IActionResult> GetPlanFeaturesListByProductIdAsync([FromRoute] Guid productId, CancellationToken cancellationToken = default)
+        {
+            return ListResult(await _planFeatureService.GetPlanFeaturesListByProductIdAsync(productId, cancellationToken));
+        }
+
+
+        [HttpPost()]
+        public async Task<IActionResult> CreatePlanFeatureAsync([FromBody] CreatePlanFeatureModel model, [FromRoute] Guid productId, CancellationToken cancellationToken = default)
+        {
+            return ItemResult(await _planFeatureService.CreatePlanFeatureAsync(model, productId, cancellationToken));
+        }
+
+
+        [HttpPut("{planFeatureId}")]
+        public async Task<IActionResult> UpdatePlanFeatureAsync([FromBody] UpdatePlanFeatureModel model, [FromRoute] Guid planFeatureId, [FromRoute] Guid productId, [FromRoute] Guid featureId, CancellationToken cancellationToken = default)
+        {
+            return EmptyResult(await _planFeatureService.UpdatePlanFeatureAsync(planFeatureId, model, productId, cancellationToken));
+        }
+
+
+        [HttpDelete("{planFeatureId}")]
+        public async Task<IActionResult> DeletePlanFeatureAsync([FromRoute] Guid planFeatureId, [FromRoute] Guid productId, [FromRoute] Guid featureId, CancellationToken cancellationToken = default)
+        {
+            return EmptyResult(await _planFeatureService.DeletePlanFeatureAsync(planFeatureId, productId, cancellationToken));
+        }
+
+        #endregion
+
+
+    }
+}
