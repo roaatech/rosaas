9f1af833320e2cc4745a5b3a174304d1e6a2feca
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2023-08-06 20:49:03 +0300

    RequestBroker enhancement

--- src/Roaa.Rosas.RequestBroker/IRequestBroker.cs
+++ src/Roaa.Rosas.RequestBroker/IRequestBroker.cs
@@ -5,17 +5,17 @@ namespace Roaa.Rosas.RequestBroker
     public interface IRequestBroker
     {
         #region Requests
-        public Task<RequestResult<TResult>> GetAsync<TResult, TRequest>(RequestModel<TRequest> requestModel);
+        public Task<RequestResult<TResult>> GetAsync<TResult, TRequest>(RequestModel<TRequest> requestModel, CancellationToken cancellationToken = default);
 
-        public Task<RequestResult<TResult>> GetAsync<TResult>(string uri);
+        public Task<RequestResult<TResult>> GetAsync<TResult>(string uri, CancellationToken cancellationToken = default);
 
-        public Task<RequestResult<TResult>> PostAsync<TResult, TRequest>(RequestModel<TRequest> requestModel);
+        public Task<RequestResult<TResult>> PostAsync<TResult, TRequest>(RequestModel<TRequest> requestModel, CancellationToken cancellationToken = default);
 
-        public Task<RequestResult<TResult>> PutAsync<TResult, TRequest>(RequestModel<TRequest> requestModel);
+        public Task<RequestResult<TResult>> PutAsync<TResult, TRequest>(RequestModel<TRequest> requestModel, CancellationToken cancellationToken = default);
 
-        public Task<RequestResult<TResult>> DeleteAsync<TResult, TRequest>(RequestModel<TRequest> requestModel);
+        public Task<RequestResult<TResult>> DeleteAsync<TResult, TRequest>(RequestModel<TRequest> requestModel, CancellationToken cancellationToken = default);
 
-        public Task<RequestResult<TResult>> DeleteAsync<TResult>(string uri);
+        public Task<RequestResult<TResult>> DeleteAsync<TResult>(string uri, CancellationToken cancellationToken = default);
         #endregion
 
 
