dc6b274e4e997adf702693f4e3098dd11c37916c
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2023-08-27 11:25:15 +0300

    added db migration

---
+++ src/Roaa.Rosas.Infrastructure/Persistence/Migrations/Identity/20230827082446_AddTenantProcessHistoryMigration.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Roaa.Rosas.Infrastructure.Persistence.Migrations.Identity
+{
+    /// <inheritdoc />
+    public partial class AddTenantProcessHistoryMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "rosas_tenant_status_history",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<long>(
+                name: "TimeStamp",
+                table: "rosas_tenant_status_history",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HealthyCount",
+                table: "rosas_tenant_health_statuses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UnhealthyCount",
+                table: "rosas_tenant_health_statuses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "rosas_tenant_process_history",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    TenantId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    ProductId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    ProcessType = table.Column<int>(type: "int", nullable: false),
+                    Data = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    OwnerId = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci"),
+                    OwnerType = table.Column<int>(type: "int", nullable: false),
+                    ProcessDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    TimeStamp = table.Column<long>(type: "bigint", nullable: false),
+                    Enabled = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rosas_tenant_process_history", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "rosas_tenant_process_history");
+
+            migrationBuilder.DropColumn(
+                name: "TimeStamp",
+                table: "rosas_tenant_status_history");
+
+            migrationBuilder.DropColumn(
+                name: "HealthyCount",
+                table: "rosas_tenant_health_statuses");
+
+            migrationBuilder.DropColumn(
+                name: "UnhealthyCount",
+                table: "rosas_tenant_health_statuses");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Created",
+                table: "rosas_tenant_status_history",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+        }
+    }
+}
