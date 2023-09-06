f56c14010ef5a4b9b8f313cf29532f063a2338e1
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2023-09-06 09:24:24 +0300

    Enhance GetTenantsPaginatedListQueryHandler

--- src/Roaa.Rosas.Application/Services/Management/Tenants/Queries/GetTenantsPaginatedList/TenantListItemDto.cs
+++ src/Roaa.Rosas.Application/Services/Management/Tenants/Queries/GetTenantsPaginatedList/TenantListItemDto.cs
@@ -1,5 +1,4 @@
-﻿using Roaa.Rosas.Common.Models;
-using Roaa.Rosas.Domain.Enums;
+﻿using Roaa.Rosas.Domain.Enums;
 
 namespace Roaa.Rosas.Application.Services.Management.Tenants.Queries.GetTenantsPaginatedList
 {
@@ -8,9 +7,16 @@ namespace Roaa.Rosas.Application.Services.Management.Tenants.Queries.GetTenantsP
         public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string UniqueName { get; set; } = string.Empty;
-        public IEnumerable<LookupItemDto<Guid>> Products { get; set; } = new List<LookupItemDto<Guid>>();
+        public IEnumerable<SubscriptionDto> Subscriptions { get; set; } = new List<SubscriptionDto>();
         public TenantStatus Status { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime EditedDate { get; set; }
+    }
+
+    public record SubscriptionDto
+    {
+        public Guid SubscriptionId { get; set; }
+        public Guid ProductId { get; set; }
+        public string? ProductName { get; set; }
     }
 }
