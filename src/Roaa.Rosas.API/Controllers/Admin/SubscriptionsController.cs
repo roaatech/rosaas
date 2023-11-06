ce6da7bf83a2a68208f0c7bb670523e86dc32d70
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2023-11-06 18:34:30 +0300

    - Implemented  admin endpoint to Reset Subscription. - Implemented  admin endpoint to Reset Subscription's Features Limits .

---
+++ src/Roaa.Rosas.API/Controllers/Admin/SubscriptionsController.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Roaa.Rosas.Application.Services.Management.Tenants.Commands.ResetSubscription;
+using Roaa.Rosas.Application.Services.Management.Tenants.Commands.ResetSubscriptionFeatureLimit;
+using Roaa.Rosas.Application.Services.Management.Tenants.Service;
+using Roaa.Rosas.Authorization.Utilities;
+using Roaa.Rosas.Framework.Controllers.Common;
+
+namespace Roaa.Rosas.Framework.Controllers.Admin
+{
+    public class SubscriptionsController : BaseSuperAdminMainApiController
+    {
+        #region Props 
+        private readonly ILogger<SubscriptionsController> _logger;
+        private readonly ITenantService _tenantService;
+        private readonly IIdentityContextService _identityContextService;
+        private readonly IWebHostEnvironment _environment;
+        private readonly ISender _mediator;
+        #endregion
+
+        #region Corts
+        public SubscriptionsController(ILogger<SubscriptionsController> logger,
+                                IWebHostEnvironment environment,
+                                IIdentityContextService identityContextService,
+                                ITenantService tenantService,
+                                ISender mediator)
+        {
+            _logger = logger;
+            _environment = environment;
+            _identityContextService = identityContextService;
+            _tenantService = tenantService;
+            _mediator = mediator;
+        }
+        #endregion
+
+        #region Actions   
+
+
+        [HttpPost("Reset")]
+        public async Task<IActionResult> ResetSubscriptionAsync([FromBody] ResetSubscriptionCommand command, CancellationToken cancellationToken = default)
+        {
+            return EmptyResult(await _mediator.Send(command, cancellationToken));
+        }
+
+
+        [HttpPost("Features/Reset")]
+        public async Task<IActionResult> ResetSubscriptionFeaturesLimitsAsync([FromBody] ResetSubscriptionFeatureLimitCommand command, CancellationToken cancellationToken = default)
+        {
+            return EmptyResult(await _mediator.Send(command, cancellationToken));
+        }
+
+        #endregion 
+    }
+}
