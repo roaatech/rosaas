75ebd360a9514c2104659caf3a066ac74ae6460a
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2023-11-07 15:50:56 +0300

    - Implemented and Developed CancelSubscriptionAutoRenewal service.

---
+++ src/Roaa.Rosas.Application/Services/Management/Tenants/Commands/CancelSubscriptionAutoRenewal/CancelSubscriptionAutoRenewalCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Roaa.Rosas.Common.Models.Results;
+
+namespace Roaa.Rosas.Application.Services.Management.Tenants.Commands.CancelSubscriptionAutoRenewal;
+public record CancelSubscriptionAutoRenewalCommand : IRequest<Result>
+{
+    public Guid SubscriptionId { get; set; }
+    public string? Comment { get; init; }
+
+
+    public CancelSubscriptionAutoRenewalCommand() { }
+
+    public CancelSubscriptionAutoRenewalCommand(Guid subscriptionId, string comment)
+    {
+        SubscriptionId = subscriptionId;
+        Comment = comment;
+    }
+}
