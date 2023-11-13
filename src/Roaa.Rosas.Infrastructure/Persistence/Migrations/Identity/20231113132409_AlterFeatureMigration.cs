b17f45f57d1601c001c220fd2d65572ee8dbe32d
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2023-11-13 16:24:47 +0300

    added migration to DbContext

---
+++ src/Roaa.Rosas.Infrastructure/Persistence/Migrations/Identity/20231113132409_AlterFeatureMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Roaa.Rosas.Infrastructure.Persistence.Migrations.Identity
+{
+    /// <inheritdoc />
+    public partial class AlterFeatureMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayOrder",
+                table: "rosas_features",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "rosas_features");
+        }
+    }
+}
