cba00ed57e35ae37ee80eab95c77bfebaae85cd7
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2024-01-14 13:06:16 +0300

    added-migration to db

---
+++ src/Roaa.Rosas.Infrastructure/Persistence/Migrations/Identity/20240114100528_AlternativePlanPriceIdDbMigration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Roaa.Rosas.Infrastructure.Persistence.Migrations.Identity
+{
+    /// <inheritdoc />
+    public partial class AlternativePlanPriceIdDbMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AlternativePlanID",
+                table: "rosas_plans",
+                newName: "AlternativePlanId");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AlternativePlanPriceId",
+                table: "rosas_plans",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AlternativePlanPriceId",
+                table: "rosas_plans");
+
+            migrationBuilder.RenameColumn(
+                name: "AlternativePlanId",
+                table: "rosas_plans",
+                newName: "AlternativePlanID");
+        }
+    }
+}
