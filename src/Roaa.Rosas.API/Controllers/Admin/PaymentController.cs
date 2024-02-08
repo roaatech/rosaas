d4e8b8b1d20c56aff177f79f1706399c7723c238
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2024-02-08 14:25:51 +0300

    Allow Anonymous to Checkout endpoint

--- src/Roaa.Rosas.API/Controllers/Admin/PaymentController.cs
+++ src/Roaa.Rosas.API/Controllers/Admin/PaymentController.cs
@@ -1,15 +1,14 @@
-﻿using IdentityServer4.AccessTokenValidation;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Roaa.Rosas.Application.Payment.Models;
 using Roaa.Rosas.Application.Payment.Services;
-using Roaa.Rosas.Authorization.Utilities;
 using Roaa.Rosas.Framework.Controllers.Common;
 
 namespace Roaa.Rosas.Framework.Controllers.Admin
 {
     [Route("api/payment/v1")]
-    [Authorize(Policy = AuthPolicy.Identity.Account, AuthenticationSchemes = IdentityServerAuthenticationDefaults.AuthenticationScheme)]
+    [AllowAnonymous]
+    // [Authorize(Policy = AuthPolicy.Identity.Account, AuthenticationSchemes = IdentityServerAuthenticationDefaults.AuthenticationScheme)]
     public class PaymentController : BaseRosasApiController
     {
         #region Props 
