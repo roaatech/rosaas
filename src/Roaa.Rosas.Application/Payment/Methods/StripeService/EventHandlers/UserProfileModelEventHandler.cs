8acd952bf7a7ec7bf867727aad36f67fb0004248
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2024-02-25 12:51:52 +0300

    - Implemented and Developed  UserProfileModelEventHandler to update customer in Stripe

---
+++ src/Roaa.Rosas.Application/Payment/Methods/StripeService/EventHandlers/UserProfileModelEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Roaa.Rosas.Application.Interfaces;
+using Roaa.Rosas.Domain.Events.Management;
+
+namespace Roaa.Rosas.Application.Payment.Methods.StripeService.EventHandlers
+{
+    public class UserProfileModelEventHandler : IInternalDomainEventHandler<UserProfileModelEvent>
+    {
+        private readonly ILogger<UserProfileModelEventHandler> _logger;
+        private readonly IStripePaymentMethodService _stripePaymentMethodService;
+
+
+        public UserProfileModelEventHandler(IStripePaymentMethodService stripePaymentMethodService,
+                                                     ILogger<UserProfileModelEventHandler> logger)
+        {
+            _stripePaymentMethodService = stripePaymentMethodService;
+            _logger = logger;
+        }
+
+
+        public async Task Handle(UserProfileModelEvent @event, CancellationToken cancellationToken)
+        {
+            await _stripePaymentMethodService.UpdateCustomerAsync(@event.UpdatedProfile.FullName, @event.UpdatedProfile.MobileNumber, @event.UserId, cancellationToken);
+        }
+    }
+}
